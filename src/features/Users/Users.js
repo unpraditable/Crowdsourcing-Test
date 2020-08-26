acd880d28805f10fc6fccb2f7eef63273118e886
Aditya Pradita S <pradita397@gmail.com>
2020-08-26 16:16:25 +0700

    Add Users component that retrieve data from external API with axios

---
+++ src/features/Users/Users.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'; 
+import axios from 'axios';
+
+class Users extends Component {
+    state = {
+        users: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://iunaptk810.execute-api.ap-southeast-1.amazonaws.com/dev/api/users`)
+        .then(res => {
+            const users = res.data;
+            this.setState({ 
+                users: users
+            });
+        })
+    }
+    
+    render() {
+
+        let users = this.state.users;
+
+        return (
+            <div className="row">
+                {/* The Users sub component */}
+                <ul className="user-card-container list-unstyled col-12">
+                    {users.map(user => 
+                        <li className="user-card">
+                            <div className="image-container">
+                                {/* Render the image from API if there is a picture of user */}
+                                {user.picture &&
+                                    <img src={`${user.picture}`} alt={`${user.name}`} />
+                                }
+                                
+                            </div>
+                            <div className="user-card--title">
+                                <p>{user.name}</p>
+                                <p>{user.company}</p>
+                            </div>
+                        </li>
+                    )}
+                </ul>
+                
+            </div>
+            
+                
+        )
+    }
+}
+
+export default Users;
